2fe9155a28d9954c90bf6738869a477249a5e9d2
hardcommitoneself <hardcommitoneself@gmail.com>
2023-12-13 17:18:53 +0800

    init commit & wip

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+declare global {
+  interface Window {
+    ethereum: any; // Adjust this type definition based on your specific use case
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Index page</h1>
+</template>
+
+<script setup lang="ts">
+import { ethers } from "ethers";
+import usdcAbi from "~/src/abi/usdc.json";
+
+// const usdcContractAddressPolygon = "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359";
+
+// const provider = new ethers.JsonRpcProvider("https://polygon.llamarpc.com");
+
+// const usdcContract = new ethers.Contract(
+//   usdcContractAddressPolygon,
+//   usdcAbi,
+//   provider
+// );
+
+// connect metamask
+if (!import.meta.env.SSR) {
+  if (window.ethereum === null) {
+    console.log("MetaMask not installed; using read-only defaults");
+  } else {
+    const ethProvider = new ethers.BrowserProvider(window.ethereum);
+
+    const signer = await ethProvider.getSigner();
+
+    const eoaAddress = await signer.getAddress();
+
+    console.log(eoaAddress);
+  }
+}
+</script>
